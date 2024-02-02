96a857bd2c8076be7e18b61141c6a2df8a151658
Ayvenou <axel.yvenou@gmail.com>
2024-02-02 17:25:28 +0100

    users table with spring sec 6

---
+++ backend/src/main/java/fr/learn/springsecurity6authserver/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package fr.learn.springsecurity6authserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    String name;
+    String email;
+    String password;
+}
